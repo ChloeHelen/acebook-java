0c369f7415de0b8d15871a5c8ed83b9ca8e8d610
Nathan Quayle <nathan.quayle@gmail.com>
2018-10-31 14:10:24 +0000

    Posts now display their timestamps

--- src/main/js/posts/post.js
+++ src/main/js/posts/post.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const Post = (props) => {
-	return (
-		<div className='post-main'>
-			<p className='post-content'>
-				{props.post.content}
-			</p>
-		</div>
-	)
+    return (
+        <div className='post-main'>
+            <p className='post-content'>
+                {props.post.content}
+            </p>
+			<p className='post-time'>
+                {props.post.createdAt}
+            </p>
+        </div>
+    )
 }
 
 export default Post;
