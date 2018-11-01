6149c1f0e8717ab7589cd9de2e9b1aec10cd71ca
Nathan Quayle <nathan.quayle@gmail.com>
2018-11-01 11:16:04 +0000

    User can sign up

---
+++ src/main/js/sign-up/signUpBuilder.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+class SignUpBuilder extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChangeName = this.handleChangeName.bind(this);
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            name: '',
+            password: '',
+            email: ''
+        }
+    }
+
+    handleChangeName(event) {
+        this.setState({
+            name: event.target.value
+        });
+    }
+
+    handleChangeEmail(event){
+        this.setState({
+            email: event.target.value
+        });
+    }
+
+    handleChangePassword(event){
+        this.setState({
+            password: event.target.value
+        });
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        fetch("/api/users", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password
+            })
+        })
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type='text' onChange={this.handleChangeName} value={this.state.name} placeholder='name' required/>
+                <input type='email' onChange={this.handleChangeEmail} value={this.state.email} placeholder='email' required/>
+                <input type='password' onChange={this.handleChangePassword} value={this.state.password} placeholder='password' required/>
+                <button>Submit</button>
+            </form>
+        );
+    }
+}
+
+export default SignUpBuilder;
